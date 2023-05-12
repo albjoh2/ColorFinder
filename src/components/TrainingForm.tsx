d937fcfc5670ea9919ae49f1582e395bfda71bc9
albjoh <aljo940616@gmail.com>
2023-05-12 15:22:15 +0200

    refining the training of the network with fewer itterations and the goal of higher diversity between nice colors

--- src/components/TrainingForm.tsx
+++ src/components/TrainingForm.tsx
@@ -1,111 +1,111 @@
-"use client";
+// "use client";
 
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
-interface CanvasProps {
-  color: object | any;
-  text: string;
-}
+// interface CanvasProps {
+//   color: object | any;
+//   text: string;
+// }
 
-export default function TrainingForm(props: CanvasProps) {
-  const [colorRed, setColorRed] = useState(props.color.background.red);
-  const [colorGreen, setColorGreen] = useState(props.color.background.green);
-  const [colorBlue, setColorBlue] = useState(props.color.background.blue);
-  const [textColorRed, setTextColorRed] = useState(props.color.text.red);
-  const [textColorGreen, setTextColorGreen] = useState(props.color.text.green);
-  const [textColorBlue, setTextColorBlue] = useState(props.color.text.blue);
+// export default function TrainingForm(props: CanvasProps) {
+//   const [colorRed, setColorRed] = useState(props.color.background.red);
+//   const [colorGreen, setColorGreen] = useState(props.color.background.green);
+//   const [colorBlue, setColorBlue] = useState(props.color.background.blue);
+//   const [textColorRed, setTextColorRed] = useState(props.color.text.red);
+//   const [textColorGreen, setTextColorGreen] = useState(props.color.text.green);
+//   const [textColorBlue, setTextColorBlue] = useState(props.color.text.blue);
 
-  const [easyToRead, setEasyToRead] = useState(0.5);
-  const [beauty, setBeauty] = useState(0.5);
+//   const [easyToRead, setEasyToRead] = useState(0.5);
+//   const [beauty, setBeauty] = useState(0.5);
 
-  useEffect(() => {
-    setColorRed(props.color.background.red);
-    setColorGreen(props.color.background.green);
-    setColorBlue(props.color.background.blue);
-    setTextColorRed(props.color.text.red);
-    setTextColorGreen(props.color.text.green);
-    setTextColorBlue(props.color.text.blue);
-  }, [props.color]);
+//   useEffect(() => {
+//     setColorRed(props.color.background.red);
+//     setColorGreen(props.color.background.green);
+//     setColorBlue(props.color.background.blue);
+//     setTextColorRed(props.color.text.red);
+//     setTextColorGreen(props.color.text.green);
+//     setTextColorBlue(props.color.text.blue);
+//   }, [props.color]);
 
-  const handleSubmit = async (event: any) => {
-    event.preventDefault();
+//   const handleSubmit = async (event: any) => {
+//     event.preventDefault();
 
-    // Create an object with the data you want to write to the JSON file
-    const data = {
-      easyToRead,
-      beauty,
-      colorRed,
-      colorGreen,
-      colorBlue,
-      textColorRed,
-      textColorGreen,
-      textColorBlue,
-    };
+//     // Create an object with the data you want to write to the JSON file
+//     const data = {
+//       easyToRead,
+//       beauty,
+//       colorRed,
+//       colorGreen,
+//       colorBlue,
+//       textColorRed,
+//       textColorGreen,
+//       textColorBlue,
+//     };
 
-    await fetch("http://localhost:8090/api/writeData", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
+//     await fetch("http://localhost:8090/api/writeData", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(data),
+//     });
 
-    setBeauty(0.5);
-    setEasyToRead(0.5);
+//     setBeauty(0.5);
+//     setEasyToRead(0.5);
 
-    window.location.reload();
-  };
+//     window.location.reload();
+//   };
 
-  return (
-    <div className="relative w-full h-120 left-0 top-0 justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 rounded-md lg:p-4 lg:dark:bg-zinc-800/30 p-20">
-      <form
-        style={{
-          width: "300px",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          gap: "1rem",
-        }}
-        onSubmit={handleSubmit}
-      >
-        <h3>What do you think?</h3>
-        <div>
-          <label>
-            Easy to read?
-            <input
-              type="range"
-              defaultValue={easyToRead}
-              onChange={(e) => setEasyToRead(+e.target.value)}
-              min={0}
-              max={0.9999999}
-              step={0.001}
-              name="easyToRead"
-            />
-          </label>
-          <p style={{ textAlign: "end", marginRight: "55px", color: "#aaa" }}>
-            {(easyToRead * 100).toFixed(0)}%
-          </p>
-        </div>
-        <div>
-          <label>
-            Beautiful?
-            <input
-              type="range"
-              defaultValue={beauty}
-              onChange={(e) => setBeauty(+e.target.value)}
-              min={0}
-              max={0.9999999}
-              step={0.001}
-              name="beauty"
-            />
-          </label>
-          <p style={{ textAlign: "end", marginRight: "55px", color: "#aaa" }}>
-            {(beauty * 100).toFixed(0)}%
-          </p>
-        </div>
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
-  );
-}
+//   return (
+//     <div className="relative w-full h-120 left-0 top-0 justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 rounded-md lg:p-4 lg:dark:bg-zinc-800/30 p-20">
+//       <form
+//         style={{
+//           width: "300px",
+//           display: "flex",
+//           flexDirection: "column",
+//           alignItems: "center",
+//           justifyContent: "center",
+//           gap: "1rem",
+//         }}
+//         onSubmit={handleSubmit}
+//       >
+//         <h3>What do you think?</h3>
+//         <div>
+//           <label>
+//             Easy to read?
+//             <input
+//               type="range"
+//               defaultValue={easyToRead}
+//               onChange={(e) => setEasyToRead(+e.target.value)}
+//               min={0}
+//               max={0.9999999}
+//               step={0.001}
+//               name="easyToRead"
+//             />
+//           </label>
+//           <p style={{ textAlign: "end", marginRight: "55px", color: "#aaa" }}>
+//             {(easyToRead * 100).toFixed(0)}%
+//           </p>
+//         </div>
+//         <div>
+//           <label>
+//             Beautiful?
+//             <input
+//               type="range"
+//               defaultValue={beauty}
+//               onChange={(e) => setBeauty(+e.target.value)}
+//               min={0}
+//               max={0.9999999}
+//               step={0.001}
+//               name="beauty"
+//             />
+//           </label>
+//           <p style={{ textAlign: "end", marginRight: "55px", color: "#aaa" }}>
+//             {(beauty * 100).toFixed(0)}%
+//           </p>
+//         </div>
+//         <input type="submit" value="Submit" />
+//       </form>
+//     </div>
+//   );
+// }
--- src/functions/brain.tsx
+++ src/functions/brain.tsx
@@ -26,6 +26,12 @@ export async function trainNeuralNet(
 
   const data = TrainingData;
 
+  function trainingCallback(state: any) {
+    console.log("Training iteration:", state.iterations);
+    console.log("Training error:", state.error);
+    // Perform additional actions or log more information here
+  }
+
   // modify training data to include two outputs
   const trainingData = data.items.map((item: PocketbaseData) => ({
     input: [
@@ -43,7 +49,16 @@ export async function trainNeuralNet(
   }));
 
   // train the neural network
-  net.train(trainingData);
+  net.train(trainingData, {
+    // Defaults values --> expected validation
+    iterations: 10000, // the maximum times to iterate the training data --> number greater than 0
+    errorThresh: 0.03, // the acceptable error percentage from training data --> number between 0 and 1
+    learningRate: 0.2, // scales with delta to effect training rate --> number between 0 and 1
+    momentum: 0.2, // scales with next layer's change value --> number between 0 and 1
+    callback: trainingCallback, // a periodic call back that can be triggered while training --> null or function
+    callbackPeriod: 1000, // the number of iterations through the training data between callback calls --> number greater than 0
+    timeout: 100000, // the max number of milliseconds to train for --> number greater than 0. Default --> Infinity
+  });
   setNet(net);
 }
 
